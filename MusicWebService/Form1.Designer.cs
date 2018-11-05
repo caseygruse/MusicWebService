a7481f15230e2f06c446ac92a7bf2475d23f1ece
caseygruse.222 <caseygruse.222@gmail.com>
2018-11-05 14:52:23 -0800

    simple api is working

--- MusicWebService/Form1.Designer.cs
+++ MusicWebService/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtArtistName = new System.Windows.Forms.TextBox();
+            this.txtSongTitle = new System.Windows.Forms.TextBox();
+            this.txtLyrics = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Artist Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Song Title";
+            // 
+            // txtArtistName
+            // 
+            this.txtArtistName.Location = new System.Drawing.Point(379, 78);
+            this.txtArtistName.Name = "txtArtistName";
+            this.txtArtistName.Size = new System.Drawing.Size(100, 20);
+            this.txtArtistName.TabIndex = 2;
+            // 
+            // txtSongTitle
+            // 
+            this.txtSongTitle.Location = new System.Drawing.Point(379, 126);
+            this.txtSongTitle.Name = "txtSongTitle";
+            this.txtSongTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtSongTitle.TabIndex = 3;
+            // 
+            // txtLyrics
+            // 
+            this.txtLyrics.Location = new System.Drawing.Point(12, 224);
+            this.txtLyrics.Multiline = true;
+            this.txtLyrics.Name = "txtLyrics";
+            this.txtLyrics.ReadOnly = true;
+            this.txtLyrics.Size = new System.Drawing.Size(776, 214);
+            this.txtLyrics.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(303, 168);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 39);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtLyrics);
+            this.Controls.Add(this.txtSongTitle);
+            this.Controls.Add(this.txtArtistName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtArtistName;
+        private System.Windows.Forms.TextBox txtSongTitle;
+        private System.Windows.Forms.TextBox txtLyrics;
+        private System.Windows.Forms.Button btnSearch;
     }
 }
 
--- MusicWebService/Form1.cs
+++ MusicWebService/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http.Headers;
 
 namespace MusicWebService
 {
@@ -15,6 +17,36 @@ namespace MusicWebService
         public Form1()
         {
             InitializeComponent();
+            //attatching the web api address to the client.
+            client.BaseAddress = new Uri("https://api.lyrics.ovh/v1/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            //telling service how we accept data  ( json response. )
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //the app that is making the request.
+            client.DefaultRequestHeaders.Add("User-Agent", "My c# App");
+        }
+
+        //client communicates with web service.
+        HttpClient client = new HttpClient();
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string artist = txtArtistName.Text;
+            string songTitle = txtSongTitle.Text;
+            //"https://api.lyrics.ovh/v1/artist/songTitle"
+            HttpResponseMessage response = client.GetAsync($"{artist}/{songTitle}").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string output = response.Content.ReadAsStringAsync().Result;
+                output = output.Replace($"\\n", " ");
+                txtLyrics.Text = output;
+            }
+            else
+            {
+                txtLyrics.Text = "Not Found";
+            }
         }
     }
 }
